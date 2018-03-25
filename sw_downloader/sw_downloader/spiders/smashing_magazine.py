215b28f09578347e3e5d38d394d6c389e990aeaa
Artem Ezhov <raystlin.bd@gmail.com>
2018-03-25 04:02:44 +0300

    spider was implemented

---
+++ sw_downloader/sw_downloader/spiders/smashing_magazine.py
@@ -0,0 +1,49 @@
+"""SmashingMagazine Wallpaper spider
+
+Finds and downloading wallpapers on smashingmagazine.com with
+specified parameters.
+
+"""
+import datetime
+
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.http import Request
+from scrapy.linkextractors import LinkExtractor
+from furl import furl
+
+
+class SmashingMagazineSpider(CrawlSpider):
+    name = 'smashingmagazine_spider'
+    start_urls = ['https://www.smashingmagazine.com/category/wallpapers/']
+    rules = (
+        Rule(
+            LinkExtractor(allow=r"/wallpapers/"),
+            callback='parse_posts_page'
+        ),
+    )
+
+    def __init__(self, year, month, resolution, *args, **kwargs):
+        super(SmashingMagazineSpider, self).__init__(*args, **kwargs)
+        self.year = year
+        self.month = month
+        self.resolution = resolution
+
+    def parse_wallpaper_page(self, response):
+        links = response.css('.c-garfield-the-cat>ul>li>a::attr(href)'
+                             ).extract()
+        for link in links:
+            if self.resolution in link and\
+               ('.png' in link or '.jpg' in link):
+                yield {'image_urls': [link]}
+
+    def parse_posts_page(self, response):
+        posts = response.css('.article--post')
+        for post in posts:
+            date_stamp = post.css('div>p>time::attr(datetime)').extract_first()
+            date = datetime.datetime.strptime(date_stamp, "%Y-%m-%d")
+            if date.month == self.month and date.year == self.year:
+                path = post.css('h1>a::attr(href)').extract_first()
+                parsed_url = furl(response.url)
+                parsed_url.path = path
+                yield Request(url=parsed_url.tostr(),
+                              callback=self.parse_wallpaper_page)
---
+++ tests/test_spider.py
@@ -0,0 +1,44 @@
+import requests
+
+from scrapy.http import HtmlResponse
+from sw_downloader.sw_downloader.spiders.smashing_magazine \
+    import SmashingMagazineSpider
+
+
+class TestSmashingMagazineSpider:
+
+    def test_ruleset(self):
+        url = ('https://www.smashingmagazine.com/category/wallpapers')
+        response = requests.get(url)
+        spider = SmashingMagazineSpider(year=2018, month=2,
+                                        resolution='1024x768')
+        scrapy_response = HtmlResponse(body=response.content, url=url)
+        links_gen = spider._requests_to_follow(scrapy_response)
+        links = list(links_gen)
+        assert len(links) == 3
+
+    def test_image_link_extraction(self):
+        url = ('https://www.smashingmagazine.com/'
+               '2018/01/desktop-wallpaper-'
+               'calendars-february-2018/')
+
+        spider = SmashingMagazineSpider(year=2018, month=1,
+                                        resolution='1024x768')
+        response = requests.get(url)
+        scrapy_response = HtmlResponse(body=response.content, url=url)
+        items_gen = spider.parse_wallpaper_page(scrapy_response)
+        items = list(items_gen)
+        assert len(items) == 26
+        for item in items:
+            assert '1024x768.jpg' in item['image_urls'][0] or \
+                   '1024x768.png' in item['image_urls'][0]
+
+    def test_extract_posts(self):
+        url = ('https://www.smashingmagazine.com/category/wallpapers')
+        spider = SmashingMagazineSpider(year=2018, month=2,
+                                        resolution='1024x768')
+        response = requests.get(url)
+        scrapy_response = HtmlResponse(body=response.content, url=url)
+        posts_gen = spider.parse_posts_page(scrapy_response)
+        posts = list(posts_gen)
+        assert len(posts) == 1
