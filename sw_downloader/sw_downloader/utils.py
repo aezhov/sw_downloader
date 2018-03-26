325f6fe98579dda4f98613e4513fc46c7bd962c7
aezhov <30422159+aezhov@users.noreply.github.com>
2018-03-26 17:38:55 +0300

    Update utils.py

--- sw_downloader/sw_downloader/utils.py
+++ sw_downloader/sw_downloader/utils.py
@@ -4,6 +4,6 @@ import datetime
 
 
 def format_dirname(year, month, resolution):
-    date_part = datetime.datetime(month=2, year=2018, day=1).strftime('%Y-%B')
+    date_part = datetime.datetime(month=month, year=year, day=1).strftime('%Y-%B')
     return "{date_part}-{resolution}".format(date_part=date_part,
                                              resolution=resolution)
